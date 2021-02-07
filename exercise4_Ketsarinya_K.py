edc97a08e22ce721f78977bfcedd7df1b0934a17
Kedsarinya Kaewsri <78682601+Puenx@users.noreply.github.com>
2021-02-07 17:54:24 +0700

    Add files via upload

---
+++ exercise4_Ketsarinya_K.py
@@ -0,0 +1,9 @@
+FE = 99
+GB = 99
+IC = 99
+CP = 99
+print("---Your Score---")
+print("Foundation English                : ", FE)
+print("General Business                  : ", GB)
+print("Introduction to Computer Systems  : ", IC)
+print("Computer Programming              : ", CP)
